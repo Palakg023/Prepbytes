61e4313f11a3d34ebd29a5a31706e20e6d4c503a
Palakg023 <91871427+Palakg023@users.noreply.github.com>
2022-04-06 19:35:24 +0530

    Add files via upload

---
+++ 6.Modulus Ten.cpp
@@ -0,0 +1,31 @@
+/*
+Modulus Ten
+
+PrepBuddy gave you a number x, and asked you to find the remainder of 
+x divided by 10 
+
+Input Format
+A single integer x is provided
+
+Output
+Print a single integer representing the required output
+
+Example
+
+Input
+1345
+
+Output
+5
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin>>a;
+	cout<<a%10;
+}
